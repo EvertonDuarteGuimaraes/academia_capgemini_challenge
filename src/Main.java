8c5fb0231500b656a85c0f2d855b96a7ca3bf6c8
EvertonDuarteGuimarães <everton.duarte@academico.ifpb.edu.br>
2022-03-06 18:37:14 -0300

    Fixing gramaticalerros

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		CallResponse responseFor = new CallResponse();
 		
-		ArrayList<Integer> questionOne = new ArrayList<>(); Collections.addAll(questionOne, 9, 2, 1, 4, 6);
-		ArrayList<Integer> questionTwo = new ArrayList<>(); Collections.addAll(questionTwo, 1, 5, 3, 4, 2);
-		String questionThree = "Olá mundo";
+		ArrayList<Integer> questionOneList = new ArrayList<>(); 
+		Collections.addAll(questionOneList, 9, 2, 1, 4, 6);
 		
-		System.out.println("Saida da primeira questão: " + responseFor.call(questionOne));
-		System.out.println("Saida da segunda questão: " + responseFor.call(questionTwo, 2));
-		System.out.println("Saida da terceira questão: " + responseFor.call(questionThree));
+		ArrayList<Integer> questionTwoList = new ArrayList<>(); 
+		Collections.addAll(questionTwoList, 1, 5, 3, 4, 2);
+		int questionTwoDiferece = 2;
+		
+		String questionThreeText = "Olá mundo";
+		
+		System.out.println("Saída da primeira questão: " + responseFor.call(questionOneList));
+		System.out.println("Saída da segunda questão: " + responseFor.call(questionTwoList, questionTwoDiferece));
+		System.out.println("Saída da terceira questão: " + responseFor.call(questionThreeText));
 	}
 }
